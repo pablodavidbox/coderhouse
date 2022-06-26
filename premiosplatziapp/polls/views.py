7b6c22befc5b05045e994e43c31a1cd8f9cc6f9d
Pablodavid <harlockraimar@gmail.com>
2022-06-26 19:21:48 -0300

    Creando la vista vote

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
-from .models import Question
+from .models import Question,Choice
+from django.urls import reverse
 
 
 def index(request):
@@ -20,6 +21,16 @@ def result(request,question_id):
     return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
 
 def vote(request,question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk= request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,"polls/detail.html",{
+                     "question":question,
+                     "error_message":"No elegiste un respuesta"
+        })
+    else:
+         selected_choice.votes += 1
+         selected_choice.save()
+         return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
     
