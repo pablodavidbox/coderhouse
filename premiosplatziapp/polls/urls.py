5e2bf25db08c80b4a8e05040a9f4f4bdc1293935
Pablodavid <harlockraimar@gmail.com>
2022-06-26 01:46:46 -0300

    Agregue un par de vistas

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
            path("",views.index,name="index"),
+           path("<int:question_id>/",views.detail,name="index"),
+           path("<int:question_id>/result/",views.result,name="index"),
+           path("<int:question_id>/vote/",views.vote,name="index"),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world ! ")
+    return HttpResponse("Estas en la pagina principal  ")
+
+def detail(request,question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}")
+
+def result(request,question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+def vote(request,question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+
+    
